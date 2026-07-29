6d8637426956a00369511ed0424730c9163d605b
Songkris <songkris@MacBook-Pro-khxng-Songkris.local>
2026-07-29 15:54:43 +0700

    เพิ่ม Test

--- TestBoundedStack.java
+++ TestBoundedStack.java
@@ -1,3 +1,22 @@
 public class TestBoundedStack {
-     
+
+    private static int passed = 0;
+    private static int failed = 0;
+
+    public static void check(boolean condition, String testName) {
+
+        if (condition) {
+            System.out.println("PASS : " + testName);
+            passed++;
+
+        } else {
+            System.out.println("FAIL : " + testName);
+            failed++;
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
