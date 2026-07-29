492d92a49d4323176b48b36a22c9a6459e27f1cb
Songkris <songkris@MacBook-Pro-khxng-Songkris.local>
2026-07-29 15:12:18 +0700

    เพิ่มตัวแปร กับ AF/RI

--- BoundedStack.java
+++ BoundedStack.java
@@ -1,3 +1,19 @@
-public class BoundedStack<TJ> {
+public class BoundedStack<TJ> { // เก็บอะไรก็ได้ int string double
+    
+    /*
+     * Abstraction Function (AF)
+       อาร์เรย์ elements[0..size-1] แทนข้อมูลทั้งหมดในstack
+       โดยข้อมูลบนสุด (Top) คือ elements[size-1]
+
+     * Representation Invariant (RI)
+       - elements ต้องไม่เป็น null
+       - capacity ต้องมากกว่า 0
+       - size ต้องมีค่าอยู่ระหว่าง 0 ถึง capacity
+       - elements.length ต้องเท่ากับ capacity
+     */
+
+private TJ[] elements;
+private int size;
+private int capacity;
 
 }
