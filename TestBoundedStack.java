333a3b47cae7ff7ea43c7106199e9ba0ac2285ab
Songkris <songkris@MacBook-Pro-khxng-Songkris.local>
2026-07-29 16:08:12 +0700

    เพิ่ม Test 1-5

--- TestBoundedStack.java
+++ TestBoundedStack.java
@@ -18,5 +18,24 @@ public class TestBoundedStack {
 
     public static void main(String[] args) {
 
+        BoundedStack<Integer> stack = new BoundedStack<>(3);
+
+        // Test 1
+        check(stack.isEmpty(), "New stack is empty");
+
+        // Test 2
+        check(stack.size() == 0, "Initial size = 0");
+
+        // Test 3
+        stack.push(10);
+            check(stack.size() == 1, "Push one item");
+
+        // Test 4
+        check(stack.peek() == 10, "Peek after one push");
+
+        // Test 5
+        stack.push(20);
+            check(stack.peek() == 20, "Peek after second push");
+
     }
 }
